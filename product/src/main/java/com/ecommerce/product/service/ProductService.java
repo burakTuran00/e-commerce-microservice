cf195cee5b2e7679ba49654a75a475b6ac5982c2
burakTuran00 <burakturan7410@gmail.com>
2026-04-05 10:24:48 +0300

    developing product service layer and its mapper.

--- product/src/main/java/com/ecommerce/product/service/ProductService.java
+++ product/src/main/java/com/ecommerce/product/service/ProductService.java
@@ -6,6 +6,7 @@ import com.ecommerce.product.dto.ProductRequest;
 import com.ecommerce.product.dto.ProductResponse;
 import com.ecommerce.product.mapper.ProductMapper;
 import com.ecommerce.product.repository.ProductRepository;
+import jakarta.persistence.EntityNotFoundException;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.jspecify.annotations.Nullable;
@@ -16,23 +17,29 @@ import java.util.List;
 @Service
 @RequiredArgsConstructor
 public class ProductService {
+
     private final ProductRepository repository;
     private final ProductMapper mapper;
 
     public Integer createProduct(ProductRequest productRequest) {
-        return null;
+        var product = mapper.ToEntity(productRequest);
+        return repository.save(product).getId();
     }
-
 
     public List<ProductPurchaseResponse> purchaseProduct(ProductPurchaseRequest productPurchaseRequest) {
         return null;
     }
 
-    public @Nullable ProductResponse getProductById(Integer productId) {
-        return null;
+    public ProductResponse getProductById(Integer productId) {
+        return repository.findById(Long.valueOf(productId))
+                .map(mapper::toProductResponse)
+                .orElseThrow(() -> new EntityNotFoundException("Product not found with id: " + productId));
     }
 
-    public @Nullable List<ProductResponse> getAllProducts() {
-        return null;
+    public List<ProductResponse> getAllProducts() {
+        return repository.findAll()
+                .stream()
+                .map(mapper::toProductResponse)
+                .toList();
     }
 }
