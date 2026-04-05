734661e2cc523925f154e3f36f15b5b17b0496ed
burakTuran00 <burakturan7410@gmail.com>
2026-04-05 12:27:53 +0300

    created order service

--- customer/src/main/java/com/ecommerce/customer/controller/CustomerController.java
+++ customer/src/main/java/com/ecommerce/customer/controller/CustomerController.java
@@ -11,7 +11,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 @RestController
-@RequestMapping("/api/api/customer")
+@RequestMapping("/api/api/customers")
 @RequiredArgsConstructor
 public class CustomerController {
     private final CustomerService customerService;
