325c270e458e4aac532f20aa427f40eaedc960ac
burakTuran00 <burakturan7410@gmail.com>
2026-04-05 10:13:25 +0300

    developing product-microservice

--- product/src/main/java/com/ecommerce/product/controller/ProductController.java
+++ product/src/main/java/com/ecommerce/product/controller/ProductController.java
@@ -1,13 +1,41 @@
 package com.ecommerce.product.controller;
 
+import com.ecommerce.product.dto.ProductPurchaseRequest;
+import com.ecommerce.product.dto.ProductPurchaseResponse;
+import com.ecommerce.product.dto.ProductRequest;
+import com.ecommerce.product.dto.ProductResponse;
 import com.ecommerce.product.service.ProductService;
+import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/api/v1/products")
 @RequiredArgsConstructor
 public class ProductController {
-    private final ProductService service;
+    private final ProductService productService;
+
+    @PostMapping
+    public ResponseEntity<Integer> createProduct(@RequestBody @Valid ProductRequest productRequest){
+        return ResponseEntity.ok(productService.createProduct(productRequest));
+    }
+
+    @PostMapping("/purchase")
+    public ResponseEntity<List<ProductPurchaseResponse>> purchaseProduct(
+            @RequestBody @Valid ProductPurchaseRequest productPurchaseRequest){
+        return ResponseEntity.ok(productService.purchaseProduct(productPurchaseRequest));
+    }
+
+    @GetMapping("/{productId}")
+    public ResponseEntity<ProductResponse> getProductById(@PathVariable("productId") Integer productId) {
+        return ResponseEntity.ok(productService.getProductById(productId));
+    }
+
+    @GetMapping
+    public ResponseEntity<List<ProductResponse>> getAllProducts() {
+            return ResponseEntity.ok(productService.getAllProducts());
+    }
 }
--- product/src/main/java/com/ecommerce/product/service/ProductService.java
+++ product/src/main/java/com/ecommerce/product/service/ProductService.java
@@ -1,13 +1,36 @@
 package com.ecommerce.product.service;
 
+import com.ecommerce.product.dto.ProductPurchaseRequest;
+import com.ecommerce.product.dto.ProductPurchaseResponse;
+import com.ecommerce.product.dto.ProductRequest;
+import com.ecommerce.product.dto.ProductResponse;
 import com.ecommerce.product.mapper.ProductMapper;
 import com.ecommerce.product.repository.ProductRepository;
+import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
+import org.jspecify.annotations.Nullable;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 @Service
 @RequiredArgsConstructor
 public class ProductService {
     private final ProductRepository repository;
     private final ProductMapper mapper;
+
+    public Integer createProduct(ProductRequest productRequest) {W
+        return null;
+    }
+
+
+    public List<ProductPurchaseResponse> purchaseProduct(ProductPurchaseRequest productPurchaseRequest) {
+        return null;
+    }
+
+    public @Nullable ProductResponse getProductById(Integer productId) {
+    }
+
+    public @Nullable List<ProductResponse> getAllProducts() {
+    }
 }
