d0c38cf53f6bcf03b1c3f58ab8dc7a43ca08e480
burakTuran00 <burakturan7410@gmail.com>
2026-04-12 10:08:37 +0300

    fixed some issues

---
+++ order/src/main/java/com/ecommerce/config/KafkaConfig.java
@@ -0,0 +1,32 @@
+package com.ecommerce.config;
+
+import com.ecommerce.kafka.OrderConfirmation;
+import com.fasterxml.jackson.databind.JsonSerializer;
+import org.apache.kafka.clients.producer.ProducerConfig;
+import org.apache.kafka.common.serialization.StringSerializer;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.kafka.core.*;
+
+import java.util.HashMap;
+import java.util.Map;
+
+@Configuration
+public class KafkaConfig {
+
+    @Bean
+    public ProducerFactory<String, OrderConfirmation> producerFactory() {
+        Map<String, Object> config = new HashMap<>();
+
+        config.put(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, "localhost:9092");
+        config.put(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, StringSerializer.class);
+        config.put(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, JsonSerializer.class);
+
+        return new DefaultKafkaProducerFactory<>(config);
+    }
+
+    @Bean
+    public KafkaTemplate<String, OrderConfirmation> kafkaTemplate() {
+        return new KafkaTemplate<>(producerFactory());
+    }
+}
--- payment/src/main/java/com/ecommerce/payment/config/KafkaPaymentTopicConfig.java
+++ payment/src/main/java/com/ecommerce/payment/config/KafkaPaymentTopicConfig.java
@@ -1,12 +1,21 @@
 package com.ecommerce.payment.config;
 
 import org.apache.kafka.clients.admin.NewTopic;
+import org.apache.kafka.clients.producer.ProducerConfig;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.kafka.annotation.EnableKafka;
 import org.springframework.kafka.config.TopicBuilder;
+import org.springframework.kafka.core.DefaultKafkaProducerFactory;
+import org.springframework.kafka.core.KafkaTemplate;
+import org.springframework.kafka.core.ProducerFactory;
+import tools.jackson.databind.ser.jdk.StringSerializer;
+
+import java.util.HashMap;
+import java.util.Map;
 
 @Configuration
-
+@EnableKafka
 public class KafkaPaymentTopicConfig {
     @Bean
     public NewTopic paymentTopic() {
